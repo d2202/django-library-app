cb4405304b33202c6fd7019a86e88e344aca75b9
Denis <denybf.wrk@gmail.com>
2021-04-06 22:52:37 +0300

    added more useful filters to admin panel

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,16 +4,30 @@ from .models import Book, Genre, Author, BookInstance, Language
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status',)
+    fieldsets = (
+     ('General info', {
+        'fields': ('book', 'imprint', 'id')
+     }),
+     ('Availablity', {
+        'fields': ('status',)
+     })
+    )
 
 
 admin.site.register(Author, AuthorAdmin)
